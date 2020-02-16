db45d4a5379a6387b50f90da723e62da5354f2b8
Jimmy Chen Chen <jchenche@uvic.ca>
2020-02-15 16:02:58 -0800

    playing with the baby snake

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/start', (request, response) => {
 
   // Response data
   const data = {
-    "color": '#0000FF',
+    "color": '#0F52BA',
     "headType": "bwc-snowman",
     "tailType": "bwc-bonhomme"
   }
@@ -35,16 +35,61 @@ app.post('/start', (request, response) => {
   return response.json(data)
 })
 
+var directions = ["up", "left", "down", "right"] // Code depends on the order of array
+
+function get_random_direction() {
+  return directions[Math.floor(Math.random() * 4)]
+}
+
+function get_snake_orientation(req) {  
+  var x_head = req.body.you.body[0].x
+  var y_head = req.body.you.body[0].y
+  var x_neck = req.body.you.body[1].x
+  var y_neck = req.body.you.body[1].y
+
+  if (x_head - x_neck > 0) {
+    return "right"
+  } else if (x_head - x_neck < 0) {
+    return "left"
+  } else if (y_head - y_neck > 0) {
+    return "down"
+  } else {
+    return "up"
+  }
+}
+
+function get_snake_reverse_orientation(req) {
+  return directions[(directions.indexOf(get_snake_orientation(req)) + 2) % 4]
+}
+
+function avoid_wall(req, data) {
+  var x_head = req.body.you.body[0].x
+  var y_head = req.body.you.body[0].y
+  var x_max = req.body.board.width - 1
+  var y_max = req.body.board.height - 1
+  var reversed_orientation = get_snake_reverse_orientation(req)
+
+  while ((reversed_orientation == data.move) || // Make sure it doesn't collide itself
+      (data.move == "up" && y_head - 1 < 0) ||
+      (data.move == "left" && x_head - 1 < 0) ||
+      (data.move == "down" && y_head + 1 > y_max) ||
+      (data.move == "right" && x_head + 1 > x_max)) {
+    data.move = get_random_direction()
+  }
+}
+
 // Handle POST request to '/move'
-app.post('/move', (request, response) => {
+app.post('/move', (req, res) => {
   // NOTE: Do something here to generate your move
 
   // Response data
   const data = {
-    move: 'up', // one of: ['up','down','left','right']
+    move: get_random_direction(), // coordinate (0,0) is at the upper left corner
   }
 
-  return response.json(data)
+  avoid_wall(req, data)
+
+  return res.json(data)
 })
 
 app.post('/end', (request, response) => {
