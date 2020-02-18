8a6c52f2b7ae31c29a7908cc9f9caf63ad58afc9
Jimmy Chen Chen <jchenche@uvic.ca>
2020-02-17 22:06:32 -0800

    fix food score transform

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function transform_battle_score(enemy_length, my_length, score) {
 
 function transform_food_score(req, score, curr_depth = 0) {
   if (req.body.turn < TIME_TO_DIET || req.body.you.health < HEALTH_THRESHOLD)
-    return score + curr_depth
+    return score + 5 + curr_depth
   return score + 1
 }
 
